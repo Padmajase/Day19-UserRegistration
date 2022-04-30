370a56797c603ec9f2658f02e253e1e5f8808e18
Padmajase <padmajapawar7@gmail.com>
2022-04-29 17:10:28 -0700

    Added : User Registration program for valid name

---
+++ User.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.day19Userregistration;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class User {
+    static Logger log = Logger.getLogger(User.class);
+    static Scanner sc = new Scanner(System.in);
+
+    static void Input() {
+        System.out.println("First Name :");
+        String firstName = sc.nextLine();
+        System.out.println((Pattern.matches("^[A-Za-z]\\w{3,}", firstName)));
+    }
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        Input();
+    }
+}
+
+
+
+
