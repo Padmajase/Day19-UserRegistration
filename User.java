e482b8714fd6b4da636cead66c0796e21bf4b8a5
Padmajase <padmajapawar7@gmail.com>
2022-04-30 18:30:33 -0700

    Added : User Registration program for all  given email pattern

--- User.java
+++ User.java
@@ -11,25 +11,30 @@ public class User {
 
     //checking user input matches with pattern or not
     static void Input() {
-        System.out.println("First Name :");
-        String firstName = sc.nextLine();
-        System.out.println((Pattern.matches("^[A-Za-z]\\w{3,}", firstName)));
-
-        System.out.println("Last Name :");
-        String lastName = sc.nextLine();
-        System.out.println((Pattern.matches("^[A-Za-z]\\w{3,}", lastName)));
+//        System.out.println("First Name :");
+//        String firstName = sc.nextLine();
+//        System.out.println((Pattern.matches("^[A-Za-z]\\w{3,}", firstName)));
+//
+//        System.out.println("Last Name :");
+//        String lastName = sc.nextLine();
+//        System.out.println((Pattern.matches("^[A-Za-z]\\w{3,}", lastName)));
+//
+//        System.out.println("Email :");
+//        String email = sc.nextLine();
+//        System.out.println((Pattern.matches("[a-zA-Z0-9_.-]+@+[a-zA-Z0-9.-]+com", email)));
+//
+//        System.out.println("phone :");
+//        String mobNumber = sc.nextLine();
+//        System.out.println((Pattern.matches("91+[\\s][1-9]{10}", mobNumber)));
+//
+//        System.out.println("password :");
+//        String password = sc.nextLine();
+//        System.out.println((Pattern.matches("[[A-Z0-9[a-z]{*}][@$._-]{1}]{8,}", password))); // matching password with exactly one special character in password
 
         System.out.println("Email :");
         String email = sc.nextLine();
-        System.out.println((Pattern.matches("[a-zA-Z0-9_.-]+@+[a-zA-Z0-9.-]+com", email)));
+        System.out.println((Pattern.matches("[a-zA-Z0-9_.-]+@+[a-zA-z-_.0-9]+[comaunet]", email)));
 
-        System.out.println("phone :");
-        String mobNumber = sc.nextLine();
-        System.out.println((Pattern.matches("91+[\\s][1-9]{10}", mobNumber)));
-
-        System.out.println("password :");
-        String password = sc.nextLine();
-        System.out.println((Pattern.matches("[[A-Z0-9[a-z]{*}][@$._-]{1}]{8,}", password))); // matching password with exactly one special character in password
     }
 
     public static void main(String[] args) {
